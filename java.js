d41ba74031004dc1aee9e7bea901541d866059d1
laisfilo <42088336+laisfilo@users.noreply.github.com>
2018-08-04 13:43:41 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+﻿function openModal(){
+    document.getElementById('myModal').style.display = "block";
+}
+function closeModal(){
+  document.getElementById('myModal').style.display = "none";
+}
+
+function openModal2(){
+    document.getElementById('myModal_2').style.display = "block";
+}
+function closeModal2(){
+  document.getElementById('myModal_2').style.display = "none";
+}
+
+function openModal3(){
+    document.getElementById('myModal_3').style.display = "block";
+}
+function closeModal3(){
+  document.getElementById('myModal_3').style.display = "none";
+}
